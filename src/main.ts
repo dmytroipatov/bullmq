214c54712a0bbf18be05122ccd371eaaef044430
dmytroipatov <56741415+awekrx@users.noreply.github.com>
2025-06-04 12:47:31 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './module';
+
+async function start() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 6666);
+}
+
+start();
